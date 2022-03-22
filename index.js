ee6ad695e6e2a37e3d8fb6a9caba60ed81261359
Elizabeth Nguyen <saper209@yahoo.com>
2022-03-22 00:26:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    return Math.abs(pickup - 42)
+}
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * 264
+}
+
+function distanceTravelledInFeet(begin, destination){
+    return Math.abs(begin - destination) * 264
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0
+    } else if (distance <= 2000){
+        return 0.02 * (distance - 400)
+    } else if (distance <= 2500){
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
